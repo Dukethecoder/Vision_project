b31f50cd9dbf69fd4706c3ccb30bdebdaa35b2b4
Duke <waituru@live.com>
2018-02-09 13:03:56 +0300

    Add files via upload

---
+++ Vision_project.cpp
@@ -0,0 +1,20 @@
+trace route to google server to show placement of firewalls thus ids
+protocols configured on the firewall rules(inbound and outbound rules)
+list of VLANs configured
+intervlan rules
+internet gate way configuration
+log review process
+NTP servers configurations
+configuration review documentation
+patch management solution daily email is sent
+
+
+4 6 5.5 6 7.5
+
+29 - upanga to DCB
+30th - upanga to aris to dcb
+	 - DCB to TPB mkwepu(TPB charge code)
+
+	 
+6 - understanding	 
+7.5 scanning
